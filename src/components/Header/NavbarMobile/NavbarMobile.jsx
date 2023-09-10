f9cd6382ba37b82b267811d4939842c5e81aec2e
Radu-A <elcastravete@gmail.com>
2023-09-10 23:02:59 +0200

    added/navbar_menu_mobile

--- src/components/Header/NavbarMobile/NavbarMobile.jsx
+++ src/components/Header/NavbarMobile/NavbarMobile.jsx
@@ -1,12 +1,41 @@
 import logo from "../../../assets/logo.svg";
 import iconHamburger from "../../../assets/icon-hamburger.svg";
+import iconClose from "../../../assets/icon-close-menu.svg";
+import { useState } from "react";
 
 const NavbarMobile = () => {
+  const [navbarIcon, setNavbarIcon] = useState(iconHamburger);
+  const [navbarClass, setNavbarClass] = useState("navbar-mobile");
+  const [mobileMenuClass, setMobileMenuClass] = useState("menu-mobile");
+  const [layerClass, setLayerClass] = useState("menu-layer");
+
+  const openMobileMenu = () => {
+    setLayerClass("menu-layer active")
+    setNavbarClass("navbar-mobile navbar-mobile-fixed");
+    setNavbarIcon(iconClose);
+    setMobileMenuClass("menu-mobile active");
+  };
+
+  const closeMobileMenu = () => {
+    setLayerClass("menu-layer")
+    setNavbarClass("navbar-mobile");
+    setNavbarIcon(iconHamburger);
+    setMobileMenuClass("menu-mobile");
+  };
+
   return (
-    <nav className="navbar-mobile">
-      <img src={logo} alt="" />
-      <img src={iconHamburger} alt="" />
-      <ul className="menu-mobile">
+    <nav className={navbarClass}>
+      <img src={logo} alt="crowdfund logo" />
+      {navbarIcon === iconHamburger ? (
+        <img
+          src={iconHamburger}
+          alt="icon-hamburger"
+          onClick={openMobileMenu}
+        />
+      ) : (
+        <img src={iconClose} alt="icon-hamburger" onClick={closeMobileMenu} />
+      )}
+      <ul className={mobileMenuClass}>
         <li className="menu-item">
           <a className="menu-link" href="#">
             About
@@ -23,7 +52,7 @@ const NavbarMobile = () => {
           </a>
         </li>
       </ul>
-      <div className="menu-layer"></div>
+      <div className={layerClass}></div>
     </nav>
   );
 };
